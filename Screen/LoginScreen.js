e26b50e94910c27fa4ce4ac813b49ebb6b9685cf
nitin <ernitinjai@gmail.com>
2020-08-13 11:03:22 +0530

    First commit

---
+++ Screen/LoginScreen.js
@@ -0,0 +1,286 @@
+/* This is an Login Registration example from https://aboutreact.com/ */
+/* https://aboutreact.com/react-native-login-and-signup/ */
+
+//Import React and Hook we needed
+import React, { useState,useEffect } from 'react';
+
+//Import all required component
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from 'react-native-google-signin';
+
+import AsyncStorage from '@react-native-community/async-storage';
+import Loader from './Components/loader';
+
+const LoginScreen = props => {
+  let [userEmail, setUserEmail] = useState('');
+  let [userPassword, setUserPassword] = useState('');
+  let [loading, setLoading] = useState(false);
+  let [errortext, setErrortext] = useState('');
+
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState([]);
+
+_signIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const {accessToken, idToken} = await GoogleSignin.signIn();
+    setloggedIn(true);
+    this._getCurrentUserInfo();
+    props.navigation.navigate('DrawerNavigationRoutes');
+  } catch (error) {
+    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      // user cancelled the login flow
+      alert('Cancel');
+    } else if (error.code === statusCodes.IN_PROGRESS) {
+      alert('Signin in progress');
+      // operation (f.e. sign in) is in progress already
+    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      alert('PLAY_SERVICES_NOT_AVAILABLE');
+      // play services not available or outdated
+    } else {
+      // some other error happened
+    }
+  }
+};
+
+_getCurrentUserInfo = async () => {
+  try {
+    const userInfo = await GoogleSignin.signInSilently();
+    console.log('User Info --> ', userInfo);
+    this.setState({ userInfo: userInfo });
+  } catch (error) {
+    if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+      alert('User has not signed in yet');
+      console.log('User has not signed in yet');
+    } else {
+      alert("Something went wrong. Unable to get user's info");
+      console.log("Something went wrong. Unable to get user's info");
+    }
+  }
+};
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId:
+        '418977770929-g9ou7r9eva1u78a3anassxxxxxxx.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    });
+  }, []);
+  signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setloggedIn(false);
+      setuserInfo([]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmitPress = () => {
+    setErrortext('');
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+    setLoading(true);
+    var dataToSend = { login: userEmail, password: userPassword };
+    var formBody = [];
+    for (var key in dataToSend) {
+      var encodedKey = encodeURIComponent(key);
+      var encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+
+    fetch('http://esunscope.org/cms/api/user/index_post', {
+      method: 'POST',
+      body: formBody,
+      headers: {
+        //Header Defination
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    }).then(response => response.json())
+      .then(responseJson => {
+        //Hide Loader
+        setLoading(false);
+        console.log(responseJson);
+        // If server response message same as Data Matched
+        if (responseJson.msg=="Successful") {
+          AsyncStorage.setItem('user_id', userEmail);
+          console.log(userEmail);
+          props.navigation.navigate('DrawerNavigationRoutes');
+        } else {
+          setErrortext('Please check your email id or password');
+          console.log('Please check your email id or password');
+        }
+      })
+      .catch(error => {
+        //Hide Loader
+        setLoading(false);
+        console.error(error);
+      });
+  };
+
+  return (
+    <View style={styles.mainBody}>
+      <Loader loading={loading} />
+      <ScrollView keyboardShouldPersistTaps="handled">
+        <View style={{ marginTop: 100 }}>
+          <KeyboardAvoidingView enabled>
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                source={require('../Images/logo.png')}
+                style={{
+                  width: '50%',
+                  height: 100,
+                  resizeMode: 'contain',
+                  margin: 30,
+                }}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={UserEmail => setUserEmail(UserEmail)}
+                //underlineColorAndroid="#FFFFFF"
+                placeholder="Enter Username" //dummy@abc.com
+                placeholderTextColor="#F6F6F7"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                ref={ref => {
+                  this._emailinput = ref;
+                }}
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  this._passwordinput && this._passwordinput.focus()
+                }
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={UserPassword => setUserPassword(UserPassword)}
+                //underlineColorAndroid="#FFFFFF"
+                placeholder="Enter Password" //12345
+                placeholderTextColor="#F6F6F7"
+                keyboardType="default"
+                ref={ref => {
+                  this._passwordinput = ref;
+                }}
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={true}
+              />
+            </View>
+            {errortext != '' ? (
+              <Text style={styles.errorTextStyle}> {errortext} </Text>
+            ) : null}
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={handleSubmitPress}>
+              <Text style={styles.buttonTextStyle}>LOGIN</Text>
+            </TouchableOpacity>
+            <Text
+              style={styles.registerTextStyle}
+              onPress={() => props.navigation.navigate('RegisterScreen')}>
+              New Here ? Register
+            </Text>
+            <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              {
+                <GoogleSigninButton
+                  style={{width: 192, height: 48}}
+                  size={GoogleSigninButton.Size.Wide}
+                  color={GoogleSigninButton.Color.Dark}
+                  onPress={this._signIn}
+                />
+              }
+            </View>
+            
+          </View>
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#307ecc',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: 'white',
+  },
+  registerTextStyle: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+});
